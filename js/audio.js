bdcc15d93e8fdff7e4d046b39555338509223596
guro1026 <hitoshi.ishiguro1026@gmail.com>
2026-05-24 12:35:32 +0900

    aaa
    aaa

--- js/audio.js
+++ js/audio.js
@@ -5,7 +5,7 @@ export const audio = {
   volume: 0,
 
   init() {
-    this.bgm = new Audio("audio/bgm.mp3");
+    this.bgm = new Audio("sounds/bgm.mp3");
     this.bgm.loop = true;
     this.bgm.volume = 0;
   },
@@ -26,25 +26,25 @@ export const audio = {
   },
 
   beep() {
-    const se = new Audio("audio/se_beep.mp3");
+    const se = new Audio("sounds/beep.mp3");
     se.volume = this.volume;
     se.play();
   },
 
   typeGood() {
-    const se = new Audio("audio/se_type_good.mp3");
+    const se = new Audio("sounds/hit.mp3");
     se.volume = this.volume;
     se.play();
   },
 
   typeMiss() {
-    const se = new Audio("audio/se_type_miss.mp3");
+    const se = new Audio("sounds/beep.mp3");
     se.volume = this.volume;
     se.play();
   },
 
   beam() {
-    const se = new Audio("audio/se_beam.mp3");
+    const se = new Audio("sounds/hit.mp3");
     se.volume = this.volume;
     se.play();
   }
